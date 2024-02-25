fabb11118566a5e7540ccdec145ad646c7b6a7bf
mondirhallouli <halloulim@gmail.com>
2024-02-25 11:41:48 +0100

    first commit, initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'softRed': 'hsl(7, 99%, 70%)',
+                'yellow': 'hsl(51, 100%, 49%)',
+                'darkDesatCyan': 'hsl(167, 40%, 24%)',
+                'darkBlue': 'hsl(198, 62%, 26%)',
+                'darkModCyan': 'hsl(168, 34%, 41%)',
+                'darkDesatBlue': 'hsl(212, 27%, 19%)',
+                'darkerGrayishBlue': 'hsl(213, 9%, 39%)',
+                'darkGrayishBlue': 'hsl(232, 10%, 55%)',
+                'grayishBlue': 'hsl(210, 4%, 67%)',
+                'white': 'hsl(0, 0%, 100%)',
+            },
+            fontFamily: {
+                'barlowFont': ['Barlow', 'sans-serif'],
+                'FrauncesFont': ['Fraunces', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
